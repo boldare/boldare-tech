81e7873e9fe6b2b39870ee8cb1185cc311449a15
damianxkozar <damian.kozar@xsolve.pl>
2018-04-17 16:51:46 +0200

    google site verification

--- src/html.js
+++ src/html.js
@@ -22,6 +22,7 @@ module.exports = class HTML extends React.Component {
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="google-site-verification" content="uuR2oHP9vAD7YFf5EWvHMYgH_6D3ZeB5D5ku_oThbYM" />
           {this.props.headComponents}
           {css}
           <meta name="apple-mobile-web-app-capable" content="yes" />
