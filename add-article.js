a521fd3459a554a3e24d7af079f4cee156826490
Krzysztof Miemiec <krzysztof.miemiec@gmail.com>
2018-08-12 16:48:20 +0200

    * Updated add-article script * Pull Request Templates on GitHub article

--- add-article.js
+++ add-article.js
@@ -1,5 +1,6 @@
 const mkdirp = require("mkdirp");
 const path = require("path");
+const readline = require("readline");
 const fs = require("fs");
 
 const POSTS_DIR = "./content/posts/";
@@ -12,53 +13,103 @@ const args = process.argv.reduce((obj, arg) => {
   return obj;
 }, {});
 
-const name = args.name;
-if (!name) {
-  console.log("Type the article name as an argument: --name=\"My new article\"");
-  process.exit(1);
-}
-const date = new Date();
-let month = (date.getMonth() + 1).toString();
-if (month.length === 1) {
-  month = "0" + month;
-}
-let day = date.getDay().toString();
-if (day.length === 1) {
-  day = "0" + day;
-}
-const dateString = date.getFullYear() + "-" + month + "-" + day;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-const slug =
-  dateString +
+const question = (argument, sample) =>
+  new Promise(resolve => {
+    rl.question(`Specify ${argument} (i.e. ${sample}): `, answer => {
+      console.log(`You can also type the article name as an argument: --${argument}="${sample}"`);
+      resolve(answer);
+    });
+  });
+
+const getDate = () => {
+  const date = new Date();
+  let month = (date.getMonth() + 1).toString();
+  if (month.length === 1) {
+    month = "0" + month;
+  }
+  let day = date.getDay().toString();
+  if (day.length === 1) {
+    day = "0" + day;
+  }
+  return date.getFullYear() + "-" + month + "-" + day;
+};
+
+const getSlug = name =>
+  getDate() +
   "--" +
   name
     .trim()
     .toLowerCase()
     .replace(/[!@#$%^&*()_+\-=\\/ ]/g, "-");
 
-const location = path.join(POSTS_DIR, slug);
-console.log("Location: " + location);
+const main = async () => {
+  // Gather data
+  let { name, subtitle, tags, author } = args;
+  if (!name) {
+    name = await question("name", "My new article");
+    if (!name) {
+      console.log("Name is required to correctly generate the article");
+      process.exit(1);
+    }
+  }
+  if (!subtitle) {
+    subtitle = await question("subtitle", "A slightly more detailed topic description.");
+  }
+  if (!tags) {
+    tags = await question("tags", "javascript, react, nodejs");
+  }
+  tags = `"${tags
+    .split(",")
+    .map(tag => tag.trim())
+    .join(`", "`)}"`;
 
-if (fs.existsSync(location)) {
-  console.log("Article already exists");
-  process.exit(2);
-}
-mkdirp.sync(location);
+  if (!author) {
+    author = await question("author", "John Doe");
+  }
 
-const template = `\
+  rl.close();
+
+  // Prepare location
+  const slug = getSlug(name);
+  const location = path.join(POSTS_DIR, slug);
+  console.log("Location: " + location);
+
+  // Create directory
+  if (fs.existsSync(location)) {
+    console.log("Article already exists");
+    process.exit(2);
+  }
+  mkdirp.sync(location);
+
+  // Prepare template
+  const template = `\
 ---
 title: ${name}
-subTitle: 
-tags: [""]
+subTitle: ${subtitle}
+tags: [${tags}]
 cover: 
-postAuthor: 
+postAuthor: ${author}
 ---
 
-- [Intro](#intro)
+- [Section 1](#section1)
 - ...
 
-## <a name="intro"></a>Intro
+## <a name="section1"></a>Section 1
 
 `;
 
-fs.writeFileSync(path.join(location, "index.md"), template, "utf8");
+  // Write template
+  fs.writeFileSync(path.join(location, "index.md"), template, "utf8");
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
