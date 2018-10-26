ea01aa34b5ed81ce717d584a123776b923523600
Mateusz Matyszkowicz <mateusz.matyszkowicz@xsolve.pl>
2018-10-26 12:47:19 +0200

    New posts and search layout

--- src/pages/search.js
+++ src/pages/search.js
@@ -3,28 +3,31 @@ import { graphql } from "gatsby";
 import PropTypes from "prop-types";
 
 require("core-js/fn/array/find");
-const path = require('path');
+const path = require("path");
 
 import Main from "../components/Main";
 import Article from "../components/Main/Article";
 import PageHeader from "../components/Page/PageHeader";
 import Search from "../components/Search";
 import Seo from "../components/Seo";
+import Layout from "../components/layout";
 
 const SearchPage = props => {
   const { data } = props;
 
   return (
-    <Main>
-      <Article>
-        <PageHeader title="Search by" algolia={true} />
-        <Search algolia={data.site.siteMetadata.algolia} />
-      </Article>
-      <Seo
-        data={{ title: "Search", slug: `/${path.basename(__filename, ".js")}` }}
-        facebook={data.site.siteMetadata.facebook}
-      />
-    </Main>
+    <Layout>
+      <Main>
+        <Article>
+          <PageHeader title="Search by" algolia={true} />
+          <Search algolia={data.site.siteMetadata.algolia} />
+        </Article>
+        <Seo
+          data={{ title: "Search", slug: `/${path.basename(__filename, ".js")}` }}
+          facebook={data.site.siteMetadata.facebook}
+        />
+      </Main>
+    </Layout>
   );
 };
 
